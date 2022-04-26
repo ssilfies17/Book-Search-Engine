2e902ab2412854ce718ef377e6322244b12da47e
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-25 20:02:29 -0700

    More debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ssilfies17:Pooperscooper23!@cluster0.hijkj.mongodb.net/booksearch_db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
